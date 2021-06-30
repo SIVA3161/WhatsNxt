4590869e4998e4bad868a61b771d9da61e35b626
SIVA3161 <sivaguru3161@gmail.com>
2021-06-30 14:18:59 +0530

    Firebase deprecated error faced

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark, primarySwatch: Colors.deepOrange),
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String todoTitle = "";
+
+  createTodos() {
+    DocumentReference documentReference =
+        // ignore: deprecated_member_use
+        Firestore.instance
+            .collection("WhatsNxt Bucket")
+            // ignore: deprecated_member_use
+            .document(todoTitle);
+
+    //Map
+    Map<String, String> todos = {"todoTitle": todoTitle};
+
+    // ignore: deprecated_member_use
+    documentReference.setData(todos).whenComplete(() {
+      print("$todoTitle created");
+    });
+  }
+
+  deleteTodos(item) {
+    DocumentReference documentReference =
+        // ignore: deprecated_member_use
+        Firestore.instance.collection("WhatsNxt Bucket").document(item);
+
+    documentReference.delete().whenComplete(() {
+      print("$item deleted");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsNxt App"),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  title: Text("New task"),
+                  content: TextField(
+                    onChanged: (String value) {
+                      todoTitle = value;
+                    },
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          createTodos();
+
+                          Navigator.of(context).pop();
+                        },
+                        child: Text("Ok"))
+                  ],
+                );
+              });
+        },
+        icon: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        label: Text("ADD NXT"),
+      ),
+      body: StreamBuilder(
+          // ignore: deprecated_member_use
+          stream: Firestore.instance.collection("WhatsNxt Bucket").snapshots(),
+          builder: (context, snapshots) {
+            if (snapshots.hasData) {
+              return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: snapshots.data.documents.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot documentSnapshot =
+                        snapshots.data.documents[index];
+                    return Dismissible(
+                        onDismissed: (direction) {
+                          deleteTodos(documentSnapshot["todoTitle"]);
+                        },
+                        key: Key(documentSnapshot["todoTitle"]),
+                        child: Card(
+                          elevation: 4,
+                          margin: EdgeInsets.all(8),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          child: ListTile(
+                            title: Text(documentSnapshot["todoTitle"]),
+                            trailing: IconButton(
+                                icon: Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ),
+                                onPressed: () {
+                                  deleteTodos(documentSnapshot["todoTitle"]);
+                                }),
+                          ),
+                        ));
+                  });
+            } else {
+              return Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
+  }
+}
